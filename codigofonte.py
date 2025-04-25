29a21670534c4cc33c1a4829e9795edaaf51d882
ProjetoAplicadoDogWalker <projetoaplicadosenaisc@gmail.com>
2025-04-25 14:50:48 -0300

    upcodigofonte

--- codigofonte.py
+++ codigofonte.py
@@ -1,3 +1,3 @@
 print('Projeto Aplicado de um App de DogWalker')
 print('Versão 1.0')
-print('Desenvolvedores:Rafael Benevides, Luciana da Silva, Michel Busquett, Camila Sixel')
+print('Desenvolvedores: Camila Sixel, Luciana da Silva, Michel Busquet, Rafael Benevides')
