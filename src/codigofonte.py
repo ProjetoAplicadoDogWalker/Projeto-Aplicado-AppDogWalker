228c43e9beb28022ec609ddbdf5a6274258508fb
ProjetoAplicadoDogAgenda <projetoaplicadosenaisc@gmail.com>
2025-10-22 22:34:04 -0300

    Update codigofonte.py

--- src/codigofonte.py
+++ src/codigofonte.py
@@ -1,3 +1,3 @@
 print('Projeto Aplicado de um App de DogWalker')
 print('Versão 1.0')
-print('Desenvolvedores: Camila Sixel, Luciana da Silva, Michel Busquet, Rafael Benevides')
+print('Desenvolvedores: Luciana da Silva, Michel Busquet, Rafael Benevides')
